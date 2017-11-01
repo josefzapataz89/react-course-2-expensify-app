de384698c867d346d39b954e6655bf1d513da648
Jose Zapata Zambrano <joferzz1989@gmail.com>
2017-10-31 22:09:39 -0400

    Expenses Summary

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseListFilter from "./ExpenseListFilter";
+import ExpensesSummary from '../components/ExpensesSummary';
 
 import ExpenseList from "./ExpenseList";
 
 const Dashboard = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilter />
         <ExpenseList />
     </div>
