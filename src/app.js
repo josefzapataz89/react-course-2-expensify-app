3b7d9ffbdeafbd1539a14863196abf951f87281b
Jose Zapata Zambrano <joferzz1989@gmail.com>
2017-11-05 09:00:44 -0400

    logout & login redirect

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -13,6 +13,14 @@ import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
+let hasRendered = false;
+const renderApp = () => {
+    if ( !hasRendered ) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
+
 const jsx = (
     <Provider store={store}>
         <AppRouter />
@@ -21,14 +29,19 @@ const jsx = (
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
 
 firebase.auth().onAuthStateChanged( ( user ) => {
     if ( user ) {
         console.log("Log In");
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if ( history.location.pathname === '/' ) {
+                history.push('/dashboard');
+            }
+        });
     } else {
         console.log("Log Out");
+        renderApp();
+        history.push('/');
     }
 });
